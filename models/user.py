505d9f18314a6c414a069ed8f65662fe406ec718
dor <dor@rest-api.com>
2023-12-25 16:24:34 +0200

    First commit!

---
+++ models/user.py
@@ -0,0 +1,16 @@
+from db import db
+
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+
+
+class RevokedTokensModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String, unique=True, nullable=False)
+    deleted_at = db.Column(db.String, nullable=False)
---
+++ resources/user.py
@@ -0,0 +1,98 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required, get_jwt
+
+from db import db
+from blocklist import BLOCKLIST
+from models import UserModel, RevokedTokensModel
+from schemas import UserSchema, RevokedTokenSchema
+
+from datetime import datetime
+
+
+blp = Blueprint("Users", "users", description="Operation on users")
+
+
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+            abort(409, message="A user with that username already exist")
+
+        user = UserModel(
+            username=user_data["username"],
+            password=pbkdf2_sha256.hash(user_data["password"])
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return {"message": "User created successfully"}, 201
+
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.username == user_data["username"]).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.id)
+            return {"access_token": access_token, "refresh_token": refresh_token}
+
+        abort(401, message="Invalid credentials")
+
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {"access_token": new_token}
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        if RevokedTokensModel.query.filter(RevokedTokensModel.token == jti).first():
+            return {"message": "looks like you already logged out"}, 403
+        token = RevokedTokensModel(token=jti, deleted_at=datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f'))
+
+        db.session.add(token)
+        db.session.commit()
+
+        return {"message": "Successfully logged out"}
+
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+
+        db.session.delete(user)
+        db.session.commit()
+
+        return {"message": "User deleted"}, 200
+
+
+@blp.route("/revoke")
+class RevokeToken(MethodView):
+    @jwt_required()
+    @blp.response(200, RevokedTokenSchema(many=True))
+    def get(self):
+        if get_jwt().get("is_admin"):
+            return RevokedTokensModel.query.all()
+        return {"message": "Admin privilege required"}, 401
+
+
