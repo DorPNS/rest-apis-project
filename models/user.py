de46ad3e66b89a6904f033fe335cce215ec06c49
dor <dor@rest-api.com>
2023-12-26 10:01:13 +0200

    postgresql server configuration

--- models/user.py
+++ models/user.py
@@ -1,16 +1,17 @@
 from db import db
 
 
-
 class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(18), nullable=False)
 
 
 class RevokedTokensModel(db.Model):
+    __tablename__ = "revoked_token"
+
     id = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.String, unique=True, nullable=False)
     deleted_at = db.Column(db.String, nullable=False)
